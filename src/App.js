8821c7d0c2c2177dd144c01ae072f87bb6bb41fd
Lara Rockenstein <lara.rockenstein@live.de>
2022-11-15 11:38:06 +0100

    load routes always on top

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './css/App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ProjectsPage from './pages/ProjectsPage';
@@ -11,10 +11,16 @@ import ProjectGiraffePage from './pages/ProjectGiraffePage';
 import ProjectAlgaePage from './pages/ProjectAlgaePage';
 import ProjectReportingPage from './pages/ProjectReportingPage';
 import ProjectPlaypenPage from './pages/ProjectPlaypenPage';
+import ScrollToTop from './helpers/ScrollToTop';
+import { useEffect } from 'react';
 
 function App() {
 
   const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
 
   const handleClick = () => {
     if (menuOpen===false) {
@@ -28,7 +34,12 @@ function App() {
   return (
     <div className="App">
 
+
+<ScrollToTop />
+
       <Routes>
+
+
         <Route path="/" element={<HomePage/>}> home </Route>
         <Route path="/projects" element={<ProjectsPage/>}> about</Route>
         <Route path="/projects/rumble" element={ <ProjectRumblePage/>}/>
@@ -38,6 +49,7 @@ function App() {
         <Route path="/projects/inflatable-playpen" element={ <ProjectPlaypenPage/>}/>
         <Route path="/about" element={<AboutPage/>}> projects </Route>
       </Routes>
+
 
       <button class="invisible-button" onClick={handleClick}>
         <Menu menuOpen={menuOpen} />
